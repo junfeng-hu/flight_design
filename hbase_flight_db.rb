f5a4a323f8a7fa958ad1118c080d97c9995852b5
junfeng_hu <grepcook@gmail.com>
2015-12-04 11:47:17 +0800

    add HBase schema design for flight-pj

---
+++ hbase_flight_db.rb
@@ -0,0 +1,29 @@
+create_namespace 'flight'
+
+user_rowkey = 'username'
+
+create 'flight:user', {NAME => 'i',
+                       BLOOMFILTER => 'ROW',
+                       BLOCKCACHE => 'true'}
+
+flight_rowkey = 'startplace:arrivalplace:flightdate:idflight'
+
+create 'flight:flight',
+       {NAME => 'a',
+        BLOOMFILTER => 'NONE',
+        BLOCKCACHE => 'false'},
+       {NAME => 't',
+        BLOOMFILTER => 'ROWCOL',
+        COMPRESSION => 'SNAPPY',
+        IN_MEMORY => 'true',
+        BLOCKCACHE => 'true'}
+
+order_rowkey = 'username:flightdate:idflight'
+
+create 'flight:order',
+       {NAME => 'o',
+        BLOOMFILTER => 'ROW',
+        COMPRESSION => 'SNAPPY',
+        BLOCKCACHE => 'false'}
+
+order_cf_o_cq = 'startplace:arrivalplace:starttime:arrivaltime:price'
